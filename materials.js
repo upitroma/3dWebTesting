a26ab35e68c27e48ecd9fd93008d0bcb9cf49629
upitroma <allen@rudolphweb.com>
2020-02-26 11:11:57 -0600

    added dev textures

--- materials.js
+++ materials.js
@@ -10,7 +10,7 @@ function PrefabMaterials(){
         flatShading: true,
     });
     this.devGrid = new THREE.MeshBasicMaterial({
-         map: new THREE.TextureLoader().load('https://threejsfundamentals.org/threejs/resources/images/wall.jpg')
+         map: new THREE.TextureLoader().load('https://lh3.googleusercontent.com/proxy/TH1FiVaB6upc9_6hrYDdPXSD98OL6ygLTRxqQWu_QxD-g-5UVHwR_zXvSzDsDISJsECoJNoeN6riEkczFI3STzXssECUKs9ypjt3FL6KgeKcrW0TuTt9brnN6lMrjkzVEt6KYjWa3toa2jo-lG3UF5n4_rh0yNgC5MxrDG0')
     });
 }
 
