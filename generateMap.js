fdb23fbed1d0bd7fb00f728bb6aaa0cc0d2e30ed
upitroma <allen@rudolphweb.com>
2020-03-05 13:43:32 -0600

    addded skybox

--- generateMap.js
+++ generateMap.js
@@ -80,6 +80,15 @@ function generateMap(){
 
 
   //skybox
-  scene.background = new PrefabMaterials().skybox
+  var geometry = new THREE.SphereBufferGeometry( 500, 60, 40 );
+  // invert the geometry on the x-axis so that all of the faces point inward
+  geometry.scale( - 1, 1, 1 );
+
+  var texture = new THREE.TextureLoader().load( './textures/skybox.jpeg' );
+  var material = new THREE.MeshBasicMaterial( { map: texture } );
+
+  mesh = new THREE.Mesh( geometry, material );
+
+  scene.add( mesh );
 
 }
--- materials.js
+++ materials.js
@@ -22,14 +22,8 @@ function PrefabMaterials(){
     });
 
 
-    this.skybox = new THREE.TextureLoader().load([
-        'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/pos-x.jpg',
-        'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/neg-x.jpg',
-        'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/pos-y.jpg',
-        'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/neg-y.jpg',
-        'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/pos-z.jpg',
-        'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/neg-z.jpg',
-    ]);
+    
+
      
 
 }
